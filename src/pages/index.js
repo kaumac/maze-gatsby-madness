40804c69f5f8456b7e8b5efbc324070a426933a9
Kaue Machado <kaumac@gmail.com>
2018-10-15 19:23:41 -0300

    WIP setup example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,42 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { user: undefined }
+  }
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState((state, props) => {
+        return { user: { id: '0001' } }
+      })
+    }, 3000)
+  }
+
+  render() {
+    return (
+      <Layout>
+        <div>
+          {!this.state.user && (
+            <div style={{ backgroundColor: 'green' }}>
+              <div style={{ backgroundColor: 'yellow' }}>
+                <p>Hello</p>
+              </div>
+            </div>
+          )}
+          {this.state.user && (
+            <div style={{ backgroundColor: 'red' }}>
+              <div style={{ backgroundColor: 'pink' }}>
+                <p>Hello Again</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage

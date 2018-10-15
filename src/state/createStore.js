35828cf1ca69f4d7b6b570532d7b67dcbd670b1f
Kaue Machado <kaumac@gmail.com>
2018-10-15 20:31:42 -0300

    Make it break

--- src/state/createStore.js
+++ src/state/createStore.js
@@ -3,13 +3,13 @@ import { createStore as reduxCreateStore } from 'redux'
 const reducer = (state, action) => {
   if (action.type === `INCREMENT`) {
     return Object.assign({}, state, {
-      count: state.count + 1,
+      count: state.count - 1,
     })
   }
   return state
 }
 
-const initialState = { count: 0 }
+const initialState = { count: typeof window === 'undefined' ? 0 : 5 }
 
 const createStore = () => reduxCreateStore(reducer, initialState)
 export default createStore
